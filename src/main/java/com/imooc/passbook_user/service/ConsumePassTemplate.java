c35aed5cf8d39509ed59d410b0e1fedd3ec80fad
Hhugoo <1403899718@qq.com>
2019-09-02 19:49:16 +0800

    add receive kafka

---
+++ src/main/java/com/imooc/passbook_user/service/ConsumePassTemplate.java
@@ -0,0 +1,50 @@
+package com.imooc.passbook_user.service;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.passbook_user.constant.Constants;
+import com.imooc.passbook_user.vo.PassTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消费kafka中的PassTemplate
+ */
+@Slf4j
+@Component
+public class ConsumePassTemplate {
+
+    @Autowired
+    private IHBasePassService iHBasePassService;
+
+    /**
+     *
+     * @param passTemplate
+     * @param key
+     * @param partition
+     * @param topic
+     */
+    @KafkaListener(topics = {Constants.TEMPLATE_TOPIC})
+    public void receive(@Payload String passTemplate,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                        @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.info("Consumer Receive PassTemplate: {}", passTemplate);
+
+        PassTemplate pt;
+
+        try {
+            pt = JSON.parseObject(passTemplate, PassTemplate.class);
+        } catch (Exception e) {
+            log.error("Parse PassTemplate Error: {}", e.getMessage());
+            return;
+        }
+
+        log.info("DropPassTemplateToHBase: {}", iHBasePassService.dropPassTemplateToHBase(pt));
+    }
+
+}
---
+++ src/main/java/com/imooc/passbook_user/service/IHBasePassService.java
@@ -0,0 +1,17 @@
+package com.imooc.passbook_user.service;
+
+import com.imooc.passbook_user.vo.PassTemplate;
+
+/**
+ * Pass HBase 服务
+ */
+public interface IHBasePassService {
+
+    /**
+     * 将PassTemplate 写入HBase
+     * @param passTemplate {@link PassTemplate}
+     * @return
+     */
+    boolean dropPassTemplateToHBase(PassTemplate passTemplate);
+
+}
