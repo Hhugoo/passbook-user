a8229fb8aaf2cfcefb827fd207b57751683edf8f
Hhugoo <1403899718@qq.com>
2019-09-06 13:16:07 +0800

    add userUsePass

--- src/main/java/com/imooc/passbook_user/service/impl/UserPassServiceImpl.java
+++ src/main/java/com/imooc/passbook_user/service/impl/UserPassServiceImpl.java
@@ -1,5 +1,6 @@
 package com.imooc.passbook_user.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.imooc.passbook_user.constant.Constants;
 import com.imooc.passbook_user.constant.PassStatus;
 import com.imooc.passbook_user.dao.MerchantsDao;
@@ -13,21 +14,15 @@ import com.imooc.passbook_user.vo.PassTemplate.BaseInfo;
 import com.imooc.passbook_user.vo.Response;
 import com.spring4all.spring.boot.starter.hbase.api.HbaseTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.filter.CompareFilter;
-import org.apache.hadoop.hbase.filter.PrefixFilter;
-import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.*;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -43,22 +38,61 @@ public class UserPassServiceImpl implements IUserPassService{
 
     @Override
     public Response getUserPassInfo(Long userId) throws Exception {
-        return null;
+        return getPassInfoByStatus(userId, PassStatus.UNUSED);
     }
 
     @Override
     public Response getUserUsedPassInfo(Long userId) throws Exception {
-        return null;
+        return getPassInfoByStatus(userId, PassStatus.USED);
     }
 
     @Override
     public Response getUserAllPassInfo(Long userId) throws Exception {
-        return null;
+        return getPassInfoByStatus(userId, PassStatus.ALL);
     }
 
     @Override
     public Response userUsePass(Pass pass) {
-        return null;
+
+        byte[] rowPrefix = Bytes.toBytes(new StringBuilder(String.valueOf(pass.getUserId())).reverse().toString());
+        Scan scan = new Scan();
+        List<Filter> filterList = new ArrayList<>();
+        filterList.add(new PrefixFilter(rowPrefix));
+        filterList.add(new SingleColumnValueFilter(
+                Constants.PassTable.FAMILY_I.getBytes(),
+                Constants.PassTable.TEMPLATE_ID.getBytes(),
+                CompareFilter.CompareOp.EQUAL,
+                Bytes.toBytes(pass.getTemplateId())
+        ));
+        filterList.add(new SingleColumnValueFilter(
+                Constants.PassTable.FAMILY_I.getBytes(),
+                Constants.PassTable.CON_DATE.getBytes(),
+                CompareFilter.CompareOp.EQUAL,
+                Bytes.toBytes("-1")
+        ));
+
+        scan.setFilter(new FilterList(filterList));
+
+        List<Pass> passes = hbaseTemplate.find(Constants.PassTable.TABLE_NAME, scan, new PassRowMapper());
+
+        if (null == pass || passes.size() != 1) {
+           log.error("UserUsePass Error: {}", JSON.toJSONString(pass));
+           return Response.failure("UserUsePass Error");
+        }
+
+        byte[] FAMILY_I = Constants.PassTable.FAMILY_I.getBytes();
+        byte[] CON_DATE = Constants.PassTable.CON_DATE.getBytes();
+
+        List<Mutation> datas = new ArrayList<>();
+        Put put = new Put(passes.get(0).getRowKey().getBytes());
+        put.addColumn(FAMILY_I, CON_DATE,
+                Bytes.toBytes(DateFormatUtils.ISO_DATE_FORMAT.format(new Date()))
+        );
+        datas.add(put);
+
+        hbaseTemplate.saveOrUpdate(Constants.PassTable.TABLE_NAME, datas.get(0));
+
+        return Response.success();
     }
 
     /**
@@ -78,16 +112,21 @@ public class UserPassServiceImpl implements IUserPassService{
                         CompareFilter.CompareOp.EQUAL : CompareFilter.CompareOp.NOT_EQUAL;
 
         Scan scan = new Scan();
+
+        List<Filter> filterList = new ArrayList<>();
+
         // 行键前缀过滤器，找到特定用户的优惠券
-        scan.setFilter(new PrefixFilter(rowPrefix));
+        filterList.add(new PrefixFilter(rowPrefix));
         // 基于列单元值的过滤器，找到未使用的优惠券
         if (status != PassStatus.ALL) {
-            scan.setFilter(new SingleColumnValueFilter(
+            filterList.add(new SingleColumnValueFilter(
                     Constants.PassTable.FAMILY_I.getBytes(),
                     Constants.PassTable.CON_DATE.getBytes(), compareOp,
                     Bytes.toBytes("-1")
             ));
         }
+
+        scan.setFilter(new FilterList(filterList));
 
         List<Pass> passes = hbaseTemplate.find(Constants.PassTable.TABLE_NAME, scan, new PassRowMapper());
         Map<String, PassTemplate> passTemplateMap = buildPassTemplateMap(passes);
